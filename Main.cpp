e39e29e319150cd09a4014d0f10d78aca9b54f8a
Shazi Awaludin <shaziawaludin23@gmail.com>
2020-04-09 09:21:26 +0700

    modified maincpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <iomanip>
+#include <windows.h>
 using namespace std;
 
-main(){
-	cout << "hello world";
+
+void login(){
+	system("color F0");
+	cout << "\n\t\t\t\t" << "|____/|==>> LOGIN <<==|\\____|";
+	cout << endl;
+	cout << "\n\t\t\t\t" << "  Username : ";
+	cout << "\n\t\t\t\t" << "  Password : ";
+	cout << endl;
+	system("pause");
+	system("cls");
 	
+}
+
+
+
+main(){
+	login();
+	
+	
+
 }
