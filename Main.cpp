b1a126b657a789233eff3879375a69d036a2ded9
Shazi Awaludin <shaziawaludin23@gmail.com>
2020-04-11 16:58:49 +0700

    menambah file pass.txt, fitur login dan acc manajement

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <iomanip>
 #include <windows.h>
+#include <fstream>
+#include <string.h>
+#include <conio.h>
 using namespace std;
 
 void gotoxy ( short x, short y )
@@ -9,7 +12,116 @@ void gotoxy ( short x, short y )
   SetConsoleCursorPosition ( GetStdHandle ( STD_OUTPUT_HANDLE ), coord );
 }
 
-void login(){
+void delete_line(const char *file_name, int n) 
+{ 
+    ifstream is(file_name); //membuka file dalam read only mode;
+    ofstream ofs; //membuka file dalam bentuk output mode
+    ofs.open("temp.txt", ofstream::out); 
+  
+    // looping untuk membaca setiap char dalam file
+    char c; 
+    int line_no = 0; 
+    while (is.get(c)) 
+    {
+        if (c == '\n') // kalau nemu karakter newline. 
+        line_no++; 
+  
+        if (line_no != n)  // isi file yang ga dihapus 
+            ofs << c; 
+    } 
+  
+    ofs.close(); // closing output file 
+    is.close();  // closing input file 
+    remove(file_name);  // remove the original file 
+    rename("temp.txt", file_name);  // rename the file 
+} 
+  
+void input_line(const char *file_name,string inputdata){
+	
+	fstream ofs;
+	ofs.open(file_name,ios::app);
+	
+	if(!ofs)
+		cout << "File file tidak dapat dibuka";
+	else{
+		ofs << "\n" << inputdata;
+	}
+	
+	ofs.close();
+}
+  
+void accvalidation(const char *FILENAME, bool *found, int *line, string VALIDATION){
+	ifstream USERACC(FILENAME);
+	string acc;
+	int loc = 21;
+	do{
+		USERACC.seekg(loc,ios::beg);
+		getline(USERACC,acc);
+		if(VALIDATION == acc)
+		{
+			(*found) = true;
+		}
+		loc += acc.length()+2;
+		(*line)++;
+	}while((*line) < 90 && !(*found));
+}
+  
+bool search_acc(const char *FILENAME,string VALIDATION){
+	string valtemp = VALIDATION;
+	bool found = false;
+	int line = 1;
+	accvalidation(FILENAME,&found,&line,valtemp);
+	return( found );
+}  
+
+int search_line(const char *FILENAME,string VALIDATION){
+	string valtemp = VALIDATION;
+	bool found = false;
+	int line = 1;
+	accvalidation(FILENAME,&found,&line,valtemp);
+	return(line-1);
+}  
+
+
+void modify(){
+	string userinput;
+	string validation;
+	
+	cout << "data yang ingin diubah >>";
+	cout << "\nUsername : ";
+	getline(cin,userinput);
+	validation += userinput + '#';
+	cout << "\nPassword : ";
+	getline(cin,userinput);
+	validation += userinput;
+			
+	int found = search_acc("pass.txt",validation);
+	int line = search_line("pass.txt",validation);
+	string databaru;
+	
+	
+	if(found && line >=1)
+		{
+			cout << "Username : ";
+			getline(cin,userinput);
+			databaru += userinput + '#';
+			cout << "\nPassword : ";
+			getline(cin,userinput);
+			databaru += userinput;
+		}
+	
+	delete_line("pass.txt",line);
+	input_line("pass.txt",databaru);
+}
+
+void login(int *LOGIN_ACCESS){
+	string userinput;
+	string validation;
+	
+	bool found = false;
+	int line = 0;
+	do{
+		validation.clear();
 	system("color F0");
 	cout << "\n\t\t\t\t" << "|____/|==>> LOGIN <<==|\\____|";
 	cout << "\n\t\t\t\t" << "|                           |";
@@ -17,10 +129,30 @@ void login(){
 	cout << "\n\t\t\t\t" << "| Password :                |";
 	cout << "\n\t\t\t\t" << "|___________________________|";
 	cout << "\n\t\t\t\t" << "|    \\_________________/    |";
+
 	cout << endl;
-	system("pause");
-	system("cls");
+	getline(cin,userinput);
+	validation += userinput + '#';
+	getline(cin,userinput);
+	validation += userinput;
+	cout << validation;
 	
+	found = search_acc("pass.txt",validation);
+	line = search_line("pass.txt",validation);
+	if(!found){
+		cout <<"\nUsername atau password tidak ditemukan";
+		cout << endl;
+		system("pause");
+		system("cls");
+	}
+	}while(!found);
+	
+	if(found && line == 1)
+	*LOGIN_ACCESS = 0;
+	else if(found && line >1)
+	*LOGIN_ACCESS = 1;
+	
+	system("cls");
 }
 
 
@@ -91,7 +223,7 @@ void exit(){
 main(){
 	int LOGIN_ACCESS = 0; //0 = admin, 1 = cashier
 	
-	//login();
+	//login(&LOGIN_ACCESS);
 	system("color 0a");
 	//logo();
 
@@ -130,6 +262,74 @@ main(){
 				}
 				case('4'):{
 					
+					char menu2;
+					system("cls");
+					cout << "USER MANAJEMENT";
+					cout << "\n1. Input data baru";
+					cout << "\n2. Edit data";
+					cout << "\n3. Hapus data";
+					cout << "\n4. Perlihatkan semua data";
+					cout << "\n\nPilih > ";
+					cin >> menu2;
+					switch(menu2){
+						case '1':{
+							string userinput;
+							string inputdata;
+							cin.ignore();
+							cout << "\nUsername : ";
+							getline(cin,userinput);
+							inputdata += userinput + '#';
+							cout << "Password : ";
+							getline(cin,userinput);
+							inputdata += userinput;
+							input_line("pass.txt",inputdata);
+						break;
+						}
+							
+						case '2':{
+							cin.ignore();
+							modify();
+						break;
+						}
+						case '3':{
+							string username,password;
+							string inputdata;
+							bool found;
+							char agreement;
+							int line;
+							do{
+								cin.ignore();
+								inputdata.clear();
+								cout << "\nUsername : ";
+								getline(cin,username);
+								inputdata += username + '#';
+								cout << "Password : ";
+								getline(cin,password);
+								inputdata += password;
+								cout << "apa kamu yakin menghapus :"
+									 << username
+									 << '?'
+									 << "\n(y/n) :";
+								cin >> agreement;
+								if(agreement == 'y' || agreement == 'Y'){
+									found = search_acc("pass.txt",inputdata);
+									line = search_line("pass.txt",inputdata);
+									if(found && line != 1){
+										delete_line("pass.txt", line);
+										cout << "\nSuccess!";
+									}
+									else if(found && line == 1)
+										cout << "\nKamu tidak bisa menghapus admin";
+										else
+											cout << "\nAkun tidak ditemukan";
+								}
+							}while(!found);
+						break;
+						}
+						case '4':{
+						break;
+						}
+					}
 				break;
 				}
 				case('0'):{
@@ -138,6 +338,8 @@ main(){
 				}
 				
 			}
+			cout << endl;
+			system("pause");
 		}while(5);
 	}
 	else if(LOGIN_ACCESS == 1){
